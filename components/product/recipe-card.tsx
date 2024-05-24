32a5edb4c85356f3e4f6a0fce582253e6ff6b4fd
Farid García <farid.herrera@gmail.com>
2024-05-24 09:39:41 -0600

    Recipe Card Component: Se corrigió una variable dentro del map

--- components/product/recipe-card.tsx
+++ components/product/recipe-card.tsx
@@ -27,7 +27,7 @@ export function RecipeCardSlideshow({ recipe }: { recipe: RecipeItem }) {
                 <Image src={step.image} layout="fill" objectFit="cover" alt="Receta" />
                 <div className="after:to-12% absolute inset-0 before:absolute before:inset-0 before:block before:h-full before:w-full before:bg-gradient-to-b before:from-black/60 before:to-transparent before:to-45% before:content-[''] after:absolute after:inset-0 after:block after:h-full after:w-full after:bg-gradient-to-b after:from-transparent after:from-45% after:to-black/80 after:content-['']">
                   <div className="border-bottom-sky absolute inset-8 z-10 flex flex-col justify-end pb-5">
-                    {idx === 0 ? (
+                    {index === 0 ? (
                       <h2 className="font-portland text-3xl font-black uppercase leading-none tracking-tighter text-[#F6E7E0]">
                         {step.text}
                       </h2>
